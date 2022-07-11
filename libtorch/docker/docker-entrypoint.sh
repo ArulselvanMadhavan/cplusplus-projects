01262595527dd7a2eaf3130e77cd1a70644a884e
Arulselvan Madhavan <arulselvan1234@gmail.com>
2022-07-10 22:44:22 -0700

    Basic tutorial getting started

--- libtorch/docker/docker-entrypoint.sh
+++ libtorch/docker/docker-entrypoint.sh
@@ -3,7 +3,8 @@ set -Eeo pipefail
 
 cmake -B build -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_PREFIX_PATH=/opt/conda/lib/python${PYTHON_VERSION}/site-packages/torch/share/cmake/Torch/ \
-      -D CREATE_SCRIPTMODULES=ON
+      -D CREATE_SCRIPTMODULES=ON \
+      -D DOWNLOAD_DATASETS=ON
 
 
 cmake --build build --target basics
--- libtorch/tutorials/basics/pytorch_basics/main.cpp
+++ libtorch/tutorials/basics/pytorch_basics/main.cpp
@@ -5,4 +5,18 @@
 
 int main() {
   std::cout << "Pytorch Basics rocks \n";
+  std::cout << std::fixed << std::setprecision(4);
+
+  // Autograd
+  std::cout << "Autograd ex:1 \n";
+  torch::Tensor x = torch::tensor(1.0, torch::requires_grad());
+  torch::Tensor w = torch::tensor(2.0, torch::requires_grad());
+  torch::Tensor b = torch::tensor(3.0, torch::requires_grad());
+
+  auto y = w * x + b;
+  y.backward();
+
+  std::cout << x.grad() << '\n';
+  std::cout << w.grad() << '\n';
+  std::cout << b.grad() << "\n\n";
 }
