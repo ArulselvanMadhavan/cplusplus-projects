ea3a303eb4d21f24e64fd4957e393ac69d694526
Arulselvan Madhavan <arulselvan1234@gmail.com>
2022-12-13 08:14:49 -0800

    save all

--- hpc/cpp/prime/prime.cc
+++ hpc/cpp/prime/prime.cc
@@ -1,16 +1,34 @@
 #include "cxxopts.hpp"
 #include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+bool check_divisor(int n, int d) {
+  auto result = (n % d) == 0;
+  return result;
+}
+
+int find_factors(int n, int d) {
+  while (d > 1) {
+    auto is_divisible = check_divisor(n, d);
+    if (is_divisible) {
+      break;
+    } else {
+      d = d - 1;
+    }
+  }
+  return d;
+}
 
 int main(int argc, char *argv[]) {
   cxxopts::Options options("prime", "prime numbers");
   options.add_options()("n,number", "Number to check for prime",
                         cxxopts::value<int>()->default_value("2"));
-  options.add_options()("d,divisor", "Number to divide",
-                        cxxopts::value<int>()->default_value("1"));
   auto result = options.parse(argc, argv);
   auto n = result["number"].as<int>();
-  auto d = result["divisor"].as<int>();
-  n = n % d;
-  std::cout << "Modulus result:" << n << '\n';
-  return 0;
+
+  auto d = std::sqrt(n);
+  d = std::ceil(d);
+  int factor0 = find_factors(n, d);
+  std::cout << factor0 << "|" << n / factor0 << '\n';
 }
